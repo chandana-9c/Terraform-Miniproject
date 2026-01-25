c2b6840aa2bf9604434bb94b8ff456e760b1f739
Ubuntu <ubuntu@ip-172-31-74-218.ec2.internal>
2026-01-25 10:42:43 +0000

    deleted vpc

--- terraform-vpc-module/envt/dev/main.tf
+++
@@ -1,8 +0,0 @@
-module "vpc" {
-  source = "../../modules/vpc"
-
-  vpc_cidr   = "10.0.0.0/16"
-  vpc_name   = "dev-vpc"
-  environment = "dev"
-}
-
--- terraform-vpc-module/envt/dev/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "dev_vpc_id" {
-  description = "VPC ID from reusable module"
-  value       = module.vpc.vpc_id
-}
-
-output "dev_vpc_cidr" {
-  description = "VPC CIDR from reusable module"
-  value       = module.vpc.vpc_cidr
-}
