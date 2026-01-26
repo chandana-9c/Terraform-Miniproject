1ef03ad1de52d068da814f0cf493cfb7ebc8f1bf
Ubuntu <ubuntu@ip-172-31-74-218.ec2.internal>
2026-01-26 04:36:08 +0000

    Updated Main - added s3 as backend

--- terraform-cicd/main.tf
+++ terraform-cicd/main.tf
@@ -1,3 +1,12 @@
+terraform { 
+  backend "s3" {
+    bucket = "s3bucketchandana0001"  # your bucket name
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
+
 resource "aws_s3_bucket" "cicd_demo" {
   bucket = "terraform-cicd-demo-123456"
   force_destroy = true
